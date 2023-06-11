936b5776a5a72b03cc92b861a1e31774e4881ba3
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-06-11 18:48:09 +1000

    Local test in node

--- test/index.js
+++ test/index.js
@@ -3,32 +3,50 @@ const { Nlp } = require('@nlpjs/nlp');
 const { LangEn } = require('@nlpjs/lang-en-min');
 
 (async () => {
-    
-    // Get data from URL (gist)
-    const modelMockUrl = "https://gist.githubusercontent.com/iosonofabio/44f816c3ad8b82c3ccc1ba7b56906c4a/raw/e8e5bcba579ff3355923435fd88a0b62bda975ae/model.nlp";
-    const modelUrl = "https://gist.githubusercontent.com/iosonofabio/c42d91f7297c949eff0168078940af2d/raw/cfc0e0cbc6265b09ef91cd06afa802e03fd7c344/model.nlp";
-    const response = await fetch(modelUrl);
-    if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-    }
-    const data = await response.text();
 
-    // Ok, got the data
-    //console.log(data);
+    let usingNode = (typeof window === 'undefined');
     
     // Prepare nlp library
     const container = await containerBootstrap();
     container.use(Nlp);
     container.use(LangEn);
     const manager = container.get('nlp');
+
+    // Retrieve model
+    let data;
+    if (usingNode) {
+        // Get data from file
+        data = require('fs').readFileSync('gists/model/model.nlp', 'utf8');
+    } else {
+        // Get data from URL (gist)
+        const modelMockUrl = "https://gist.githubusercontent.com/iosonofabio/44f816c3ad8b82c3ccc1ba7b56906c4a/raw/e8e5bcba579ff3355923435fd88a0b62bda975ae/model.nlp";
+        const modelUrl = "https://gist.githubusercontent.com/iosonofabio/c42d91f7297c949eff0168078940af2d/raw/f784bd8e77cc18706d1d8e98248106c303ed21f9/model.nlp";
+
+        let response = await fetch(modelUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        data = await response.text();
+    }
+    
+    // Import the model into manager
     await manager.import(data);
 
-    // Use the model
-    const answer = await manager.process('en', 'I should go now');
+    // Create a function to interact with the bot
+    function ask(question) {
+        return manager.process("en", question);
+    }
 
-    // Report the result
-    console.log(answer);
-
-    window.answerFun = function(question) { return manager.process("en", question) };
+    if (usingNode) {
+        exports.ask = ask;
+        exports.nlp = manager;
+        if (process.argv.length >= 2) {
+            const question = process.argv[2];
+            console.log(await ask(question));
+        }
+    } else {
+        window.ask = ask
+        window.nlp = manager;
+    }
 
 })();
