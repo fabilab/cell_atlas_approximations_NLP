32cc155e13c2789844ef8d4c4f420ef263f1abe4
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-06-14 09:09:17 +1000

    Update structure

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const { containerBootstrap } = require('@nlpjs/core');
 const { Nlp } = require('@nlpjs/nlp');
 const { LangEn } = require('@nlpjs/lang-en-min');
 
+let debug = true;
+
 // Construct an answer given the API has provided the requested information
 function phraseAnswer(intent, data) {
     function _chainList(list, sep, end) {
@@ -56,14 +58,15 @@ async function callAPI(intent, entities) {
     let endpoint = intent.split(".")[0];
 
     // Some verbosity for debug
-    console.log("API endpoint: " + endpoint + ".");
-    //console.log("Request parameters: " + JSON.stringify(params));
+    if (debug)
+        console.log("API endpoint: " + endpoint + ".");
 
     // Phrase https request (they are all GET for now, so URI encoding suffices)
     const uriSuffix = new URLSearchParams(params).toString();
     const uri = "https://api.atlasapprox.org/v1/" + endpoint + "?" + uriSuffix;
 
-    console.log("API URI: " + uri)
+    if (debug)
+        console.log("API URI: " + uri)
 
     let response = await fetch(uri);
 
@@ -77,35 +80,30 @@ async function callAPI(intent, entities) {
     // Construct a NL answer from the data. This might be better outsourced to nlpjs.
     const answer = phraseAnswer(intent, data);
 
-    return answer;
+    return {
+        intent: intent,
+        endpoint: endpoint,
+        params: params,
+        data: data,
+        answer: answer,
+    };
 }
 
 (async () => {
 
-    let usingNode = (typeof window === 'undefined');
-    
     // Prepare nlp library
     const container = await containerBootstrap();
     container.use(Nlp);
     container.use(LangEn);
     const manager = container.get('nlp');
 
-    // Retrieve model
-    let data;
-    if (usingNode) {
-        // Get data from file
-        data = require('fs').readFileSync('gists/model/model.nlp', 'utf8');
-    } else {
-        // Get data from URL (gist)
-        const modelMockUrl = "https://gist.githubusercontent.com/iosonofabio/44f816c3ad8b82c3ccc1ba7b56906c4a/raw/e8e5bcba579ff3355923435fd88a0b62bda975ae/model.nlp";
-        const modelUrl = "https://gist.githubusercontent.com/iosonofabio/c42d91f7297c949eff0168078940af2d/raw/6bc612f2f96925c3cd75767c949a6de58ed4ee6d/model.nlp";
-
-        let response = await fetch(modelUrl);
-        if (!response.ok) {
-            throw new Error(`HTTP error: ${response.status}`);
-        }
-        data = await response.text();
+    // Get data from URL (gist)
+    const modelUrl = "https://gist.githubusercontent.com/iosonofabio/c42d91f7297c949eff0168078940af2d/raw/55e6f38b8189f762518476f6d6919bde859a71ab/model.nlp";
+    let response = await fetch(modelUrl);
+    if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
     }
+    let data = await response.text();
     
     // Import the model into manager
     await manager.import(data);
@@ -114,48 +112,27 @@ async function callAPI(intent, entities) {
     async function ask(question, context = {}) {
         let response = await manager.process("en", question, context);
 
-        console.log(response);
+        if (debug)
+            console.log(response);
 
         // Check if there are slotFill, in which case the question is not well posed
         if (response.slotFill) {
-            return response.answer;
+            return {
+                answer: response.answer,
+                complete: false,
+            };
         }
 
-        console.log("calling API...");
-        let answer = await callAPI(response.intent, response.entities);
-        response.answer = answer;
+        if (debug)
+            console.log("calling API...");
+        let APIAnswer = await callAPI(response.intent, response.entities);
+        APIAnswer.complete = true;
+        response.answer = APIAnswer.answer;
 
-        return response.answer;
+        return APIAnswer;
     }
 
-    if (usingNode) {
-        exports.ask = ask;
-        exports.nlp = manager;
-        if (process.argv.length >= 2) {
-            //const question = process.argv[2];
-            const context = {};
-            let questionsGroups = [
-                //["what cell types are available?", "in mouse", "lung"],
-                ["what is the expression of Col1a1 in mouse heart"],
-                //["what is the expression of Col1a1?", "in mouse", "in heart"],
-            ];
-            for (let k = 0; k < questionsGroups.length; k++) {
-                let questions = questionsGroups[k];
-                console.log("############################################");
-                console.log("--------------------------------------------");
-                for (let i = 0; i < questions.length; i++) {
-                    let question = questions[i];
-                    console.log(question);
-                    console.log(await ask(question, context));
-                    console.log("--------------------------------------------");
-                }
-                console.log("############################################");
-            }
-
-        }
-    } else {
-        window.ask = ask
-        window.nlp = manager;
-    }
+    window.ask = ask
+    window.nlp = manager;
 
 })();
